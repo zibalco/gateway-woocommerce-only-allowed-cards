a80ca970191c680eb9f1ed3ffcc14c5bf24e95f9
= <yhy.kng@gmail.com>
2020-12-01 18:23:33 +0330

    Zibal woocommerce only allowed cards first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Zibal payment gateway for woocommerce with card limit
+Version: 1.5
+Description:  افزونه درگاه پرداخت زیبال برای فروشگاه ساز ووکامرس هراه با قابلیت امکان پرداخت فقط با شماره کارت های ثبت شده
+Plugin URI: http://zibal.ir
+Author: Mohammad Zamanzadeh
+Author URI: http://zibal.ir/
+
+*/
+include_once("class-wc-gateway-zibal.php");
